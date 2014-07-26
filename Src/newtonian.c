57e3137743beaec3529d10b0fba1f7e69a2304dd
Michael Davenport <Davenport.physics@gmail.com>
2014-07-26 11:21:42 -0500

    License change from GPL_v3 to MIT

--- Src/newtonian.c
+++ Src/newtonian.c
@@ -1,66 +1,72 @@
 /*
- * newtonian.c
- * 
- * Copyright 2013 Michael Davenport <Davenport.physics@gmail.com>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
+*    Physics Sim
+*
+*    Copyright (C) 2014 Michael Davenport <Davenport.physics@gmail.com>
+*
+*
+*   Permission is hereby granted, free of charge, to any person 
+*   obtaining a copy of this software and associated documentation 
+*   files (the "Software"), to deal in the Software without 
+*   restriction, including without limitation the rights to use, 
+*   copy, modify, merge, publish, distribute, sublicense, and/or 
+*   sell copies of the Software, and to permit persons to whom 
+*   the Software is furnished to do so, subject to the following 
+*   conditions:
+*
+*   	The above copyright notice and this permission notice shall be 
+*   	included in all copies or substantial portions of the Software.
+*
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+*   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+*   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+*   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+*   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+*   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+*   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+*   OTHER DEALINGS IN THE SOFTWARE.
+*
+*/
 
 
 #include "newtonian.h"
 
-inline long double momentum_massVelocity(long double M , long double V) {
+long double momentum_massVelocity(long double M , long double V) {
 	
 	return M * V;
 	
 }
 
-inline long double velocity_accelerationTime( long double A , long double T) {
+long double velocity_accelerationTime( long double A , long double T) {
 	
 	return A * T;
 
 }
 
-inline long double force_massAcceleration(long double M , long double A) {
+long double force_massAcceleration(long double M , long double A) {
 	
 	return M * A;
 	
 }
 
-inline long double mass_forceAcceleration( long double F , long double A ) {
+long double mass_forceAcceleration( long double F , long double A ) {
 	
 	return F / A;
 
 }
 
-inline long double acceleration_forceMass( long double F , long double M ) {
+long double acceleration_forceMass( long double F , long double M ) {
 	
 	return F / M;
 
 }
 
-inline long double metres_velocityTime( long double V , long double T ) {
+long double metres_velocityTime( long double V , long double T ) {
 	
 	return V * T;
 	
 }
 
-inline long double velocity_metresTime( long double M , long double T ) {
+long double velocity_metresTime( long double M , long double T ) {
 	
 	return M / T;
 
